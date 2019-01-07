de20f82ee40ed55e8a35fa093835a7590d7a990f
Jackson Watkins <jacksonwatkins@Jacksons-MacBook-Pro-2.local>
2019-01-06 22:39:42 -0800

    Completed reverse-integer there is another way to do it just by using math operations to push and pop digits

---
+++ python3/reverse-integer.py
@@ -0,0 +1,25 @@
+# Given a 32-bit signed integer, reverse digits of an integer.
+
+# Note:
+# Assume we are dealing with an environment which could only store integers
+# within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of
+# this problem, assume that your function returns 0 when the reversed integer
+# overflows.
+
+def reverse(self, x):
+    """
+    :type x: int
+    :rtype: int
+    """
+    result = 0
+
+    if str(x)[0] == '-':
+        result = int("-" + str(x)[:0:-1])
+    elif str(x)[:-1] == '0':
+        result = int(str(x)[:0:-1])
+    else:
+        result = int(str(x)[::-1])
+
+    if result >= 2**31 or result < -2**31:
+        return 0
+    return result
