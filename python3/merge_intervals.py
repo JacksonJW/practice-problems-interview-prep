d684b1711935f3e0cc08876211dad9b5f19d59eb
JacksonJW <jacksonjwatkins@gmail.com>
2019-08-10 15:51:16 -0700

    Leetcode #56 - Merge Intervals - Sorting and list traversal - O(n*log(n)) time, O(n) space

---
+++ python3/merge_intervals.py
@@ -0,0 +1,29 @@
+"""
+Given a collection of intervals, merge all overlapping intervals.
+
+Example 1:
+
+Input: [[1, 3], [2, 6], [8, 10], [15, 18]]
+Output: [[1, 6], [8, 10], [15, 18]]
+Explanation: Since intervals[1, 3] and [2, 6] overlaps, merge them into[1, 6].
+
+Example 2:
+Input: [[1, 4], [4, 5]]
+Output: [[1, 5]]
+Explanation: Intervals[1, 4] and [4, 5] are considered overlapping.
+NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+"""
+
+
+def merge(intervals: list):
+    result = []
+    for i in sorted(intervals, key=lambda x: x[0]):
+        if result and result[-1][1] >= i[0]:
+            result[-1][1] = i[1]
+        else:
+            result.append(i)
+    return result
+
+
+print("Expected answer should be [[1,6],[8,10],[15,18]] and the actual result is: " +
+      str(merge([[1, 3], [2, 6], [8, 10], [15, 18]])))
