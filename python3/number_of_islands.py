caea6d5a9037ca6bf27ddf94fab2e385398768ff
JacksonJW <jacksonjwatkins@gmail.com>
2019-09-19 15:27:18 -0700

    Leetcode #200 Number of Islands

--- python3/number_of_islands.py
+++ python3/number_of_islands.py
@@ -1,25 +1,86 @@
-def num_islands(grid):
-    number_of_islands = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[0])):
-            if grid[i][j] == '1':
-                number_of_islands += 1
-                part_of_island(grid, i, j)
-    return number_of_islands
+"""
+Leetcode #200 Number of Islands
+
+Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+You may assume all four edges of the grid are all surrounded by water.
+
+Example 1:
+Input:
+11110
+11010
+11000
+00000
+Output: 1
+
+Example 2:
+Input:
+11000
+11000
+00100
+00011
+Output: 3
+
+Algorithm/DS used: BFS graph traversal and hashmap to hold visited coordinates
+
+O(n) worst case time where n is the length of the 2d array
+
+O(n+q) worst case space where n is the number of coordinates in visited and q is the number of items added to the queue for BFS.
+
+"""
+import collections
+from typing import List
 
 
-def part_of_island(grid, i, j):
-    if i < 0 or j < 0 or i == len(grid) or j == len(grid[0]) or grid[i][j] == '0':
-        return
-    else:
-        grid[i][j] = '0'
-        part_of_island(grid, i+1, j)
-        part_of_island(grid, i, j+1)
-        part_of_island(grid, i-1, j)
-        part_of_island(grid, i, j-1)
+class Solution:
+    def __init__(self):
+        self.visited = set()
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        m = len(grid)
+        n = len(grid[0])
+        visited = set()
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1' and (i, j) not in visited:
+                    count += 1
+                    visited = self.traverse_island(grid, i, j, visited)
+        return count
+
+    def traverse_island(self, grid, i, j, visited):
+        q = collections.deque()
+        q.append((i, j))
+        m = len(grid)
+        n = len(grid[0])
+        visited.add((i, j))
+
+        while q:
+            i_pos, j_pos = q.popleft()
+
+            if 0 <= i_pos-1 and (i_pos-1, j_pos) not in visited and grid[i_pos-1][j_pos] == '1':
+                q.append((i_pos-1, j_pos))
+                visited.add((i_pos-1, j_pos))
+            if j_pos+1 < n and (i_pos, j_pos+1) not in visited and grid[i_pos][j_pos+1] == '1':
+                q.append((i_pos, j_pos+1))
+                visited.add((i_pos, j_pos+1))
+            if i_pos+1 < m and (i_pos+1, j_pos) not in visited and grid[i_pos+1][j_pos] == '1':
+                q.append((i_pos+1, j_pos))
+                visited.add((i_pos+1, j_pos))
+            if 0 <= j_pos-1 and (i_pos, j_pos-1) not in visited and grid[i_pos][j_pos] == '1':
+                q.append((i_pos, j_pos-1))
+                visited.add((i_pos, j_pos-1))
+
+        return visited
 
 
-g = [['1', '1', '0', '0', '0'], ['1', '1', '0', '0', '0'],
-     ['0', '0', '1', '0', '0'], ['0', '0', '0', '1', '1']]
-print("The expected result for the values is 3 and the actual result is: " +
-      str(num_islands(g)))
+def test_solution():
+    s = Solution()
+    print("Expected result from input [\"11000\", \"11000\", \"00100\", \"00011\"] is 3 and the Actual result is: " +
+          str(s.numIslands(["11000", "11000", "00100", "00011"])))
+    assert s.numIslands(["11000", "11000", "00100", "00011"]) == 3
+    assert s.numIslands(["1111", "1101", "1100", "0000"]) == 1
+
+
+if __name__ == "__main__":
+    test_solution()
