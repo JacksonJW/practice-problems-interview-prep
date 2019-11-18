9ef14224d6316bb98da61ea7e4a1e5ea78303748
JacksonJW <jacksonjwatkins@gmail.com>
2019-11-18 15:36:21 -0800

    Faster solution O(n) time

--- python3/merge_intervals.py
+++ python3/merge_intervals.py
@@ -1,29 +1,56 @@
 """
+Leetcode #56. Merge Intervals
+
 Given a collection of intervals, merge all overlapping intervals.
 
 Example 1:
 
-Input: [[1, 3], [2, 6], [8, 10], [15, 18]]
-Output: [[1, 6], [8, 10], [15, 18]]
-Explanation: Since intervals[1, 3] and [2, 6] overlaps, merge them into[1, 6].
-
+Input: [[1,3],[2,6],[8,10],[15,18]]
+Output: [[1,6],[8,10],[15,18]]
+Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
 Example 2:
-Input: [[1, 4], [4, 5]]
-Output: [[1, 5]]
-Explanation: Intervals[1, 4] and [4, 5] are considered overlapping.
-NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+
+Input: [[1,4],[4,5]]
+Output: [[1,5]]
+Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+
+Algorithm/DS used: Two pointers logic appending list
+
+O(n*log(n)) worst case time where n is the length of the input 'intervals'
+
+O(n) worst case space where n is the length of the input 'intervals'
+
 """
+from typing import List
 
 
-def merge(intervals: list):
-    result = []
-    for i in sorted(intervals, key=lambda x: x[0]):
-        if result and result[-1][1] >= i[0]:
-            result[-1][1] = i[1]
-        else:
-            result.append(i)
-    return result
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+        result = []
+        i = 0
+        while i < len(intervals):
+            current_merge = intervals[i]
+            j = i + 1
+            while j < len(intervals):
+                if intervals[j][0] <= current_merge[1]:
+                    current_merge = [current_merge[0], intervals[j][1]]
+                    j += 1
+                else:
+                    break
+            result.append(current_merge)
+            i = j
+        return result
 
 
-print("Expected answer should be [[1,6],[8,10],[15,18]] and the actual result is: " +
-      str(merge([[1, 3], [2, 6], [8, 10], [15, 18]])))
+def test_solution():
+    s = Solution()
+    print("Expected result from input [[1,3], [2,6], [8,10], [15,18]] is [[1,6], [8,10], [15,18]] and the Actual result is: " +
+          str(s.merge([[1, 3], [2, 6], [8, 10], [15, 18]])))
+    assert s.merge([[1, 3], [2, 6], [8, 10], [15, 18]]) == [
+        [1, 6], [8, 10], [15, 18]]
+
+
+if __name__ == "__main__":
+    test_solution()
