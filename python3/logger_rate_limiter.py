613adde7d3c9e6e7cf6ed7e9830065c6fa313854
JacksonJW <jacksonjwatkins@gmail.com>
2022-02-20 09:41:42 -0800

    185ms faster than 57.44%

---
+++ python3/logger_rate_limiter.py
@@ -0,0 +1,87 @@
+"""
+Leetcode #359 Logger Rate Limiter
+
+Design a logger system that receives a stream of messages along with their timestamps. 
+Each unique message should only be printed at most every 10 seconds (i.e. a message 
+printed at timestamp t will prevent other identical messages from being printed until 
+timestamp t + 10).
+
+All messages will come in chronological order. Several messages may arrive at the same 
+timestamp.
+
+Implement the Logger class:
+
+Logger() Initializes the logger object.
+bool shouldPrintMessage(int timestamp, string message) Returns true if the message 
+should be printed in the given timestamp, otherwise returns false.
+ 
+
+Example 1:
+Input
+["Logger", "shouldPrintMessage", "shouldPrintMessage", "shouldPrintMessage", 
+"shouldPrintMessage", "shouldPrintMessage", "shouldPrintMessage"]
+[[], [1, "foo"], [2, "bar"], [3, "foo"], [8, "bar"], [10, "foo"], [11, "foo"]]
+Output
+[null, true, true, false, false, false, true]
+
+Explanation
+Logger logger = new Logger();
+logger.shouldPrintMessage(1, "foo");  
+// return true, next allowed timestamp for "foo" is 1 + 10 = 11
+logger.shouldPrintMessage(2, "bar");  
+// return true, next allowed timestamp for "bar" is 2 + 10 = 12
+logger.shouldPrintMessage(3, "foo");  // 3 < 11, return false
+logger.shouldPrintMessage(8, "bar");  // 8 < 12, return false
+logger.shouldPrintMessage(10, "foo"); // 10 < 11, return false
+logger.shouldPrintMessage(11, "foo"); 
+// 11 >= 11, return true, next allowed timestamp for "foo" is 11 + 10 = 21
+ 
+Constraints:
+0 <= timestamp <= 109
+Every timestamp will be passed in non-decreasing order (chronological order).
+1 <= message.length <= 30
+At most 104 calls will be made to shouldPrintMessage.
+
+Algorithm/DS used: <ALGORITHM USED/DS USED>
+
+O(1) worst case time
+
+O(N) worst case space where N is the number of calls to shouldPrintMessage
+
+"""
+
+# Your Logger object will be instantiated and called as such:
+
+
+class Logger:
+
+    def __init__(self):
+        self.d = {}
+
+    def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
+        if message not in self.d:
+            self.d[message] = timestamp + 10
+            return True
+        elif message in self.d and timestamp >= self.d[message]:
+            self.d[message] = timestamp + 10
+            return True
+        else:
+            return False
+
+
+def test_solution():
+    logger = Logger()
+    print("Expected result from input (1, \"foo\") is True and the Actual result is: " +
+          str(logger.shouldPrintMessage(1, "foo")))     # return true, next allowed timestamp for "foo" is 1 + 10 = 11
+    # return true, next allowed timestamp for "bar" is 2 + 10 = 12
+    assert logger.shouldPrintMessage(2, "bar") == True
+    assert logger.shouldPrintMessage(3, "foo") == False  # 3 < 11, return false
+    assert logger.shouldPrintMessage(8, "bar") == False  # 8 < 12, return false
+    assert logger.shouldPrintMessage(
+        10, "foo") == False  # 10 < 11, return false
+    # 11 >= 11, return true, next allowed timestamp for "foo" is 11 + 10 = 21
+    assert logger.shouldPrintMessage(11, "foo") == True
+
+
+if __name__ == "__main__":
+    test_solution()
