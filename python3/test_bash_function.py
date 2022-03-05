f7fdfff9462af5aaf4d55bc02e2a5ddf76a7e58f
JacksonJW <jacksonjwatkins@gmail.com>
2022-03-05 11:03:51 -0800

    43 ms, faster than 53.01%

---
+++ python3/test_bash_function.py
@@ -0,0 +1,36 @@
+"""
+Leetcode #<PROBLEM NUMBER AND TITLE>
+
+<LEETCODE PROBLEM DESCRIPTION>
+
+Algorithm/DS used: <ALGORITHM USED/DS USED>
+
+<AVERAGE TIME COMPLEXITY> worst case time
+
+<AVERAGE SPACE COMPLEXITY> worst case space
+
+"""
+
+
+"""
+How to use:
+- Replace class 'Solution' with your Leetcode solution.
+- Replace calls 's.your_method_name()' with 's.<YOUR_ACTUAL_METHOD_NAME(<ARGS,>)>' in test_solution().
+"""
+
+
+class Solution:
+    def your_method_name(self):
+        pass
+
+
+def test_solution():
+    s = Solution()
+    print("Expected result from input <{INPUT_ARG(S)}> is <EXPECTED_RESULT> and the Actual result is: " +
+          str(s.your_method_name()))
+    assert s.your_method_name() == '<EXPECTED_RESULT>'
+    # Insert more tests here...
+
+
+if __name__ == "__main__":
+    test_solution()
