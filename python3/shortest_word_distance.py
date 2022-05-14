9feb0a72c4f91855095ea7bc97ec87967c45c3be
JacksonJW <jacksonjwatkins@gmail.com>
2022-05-14 12:08:53 -0700

    94 ms, faster than 44.16%

--- python3/shortest_word_distance.py
+++ python3/shortest_word_distance.py
@@ -2,21 +2,18 @@ from typing import List
 
 
 class Solution:
-    def shortestDistance(self, words: List[str], word1: str, word2: str) -> int:
-        min_distance = float('inf')
+    def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
         word1_index = -1
         word2_index = -1
-        for i in range(len(words)):
-            if words[i] == word1:
+        shortest = float('inf')
+        for i, e in enumerate(wordsDict):
+            if e == word1:
                 word1_index = i
-            elif words[i] == word2:
+            elif e == word2:
                 word2_index = i
-
-            if word1_index != -1 and word2_index != -1:
-                min_distance = min(min_distance, abs(
-                    word1_index - word2_index))
-
-        return min_distance
+            if word1_index > -1 and word2_index > -1:
+                shortest = min(shortest, abs(word1_index - word2_index))
+        return shortest
 
 
 def main():
