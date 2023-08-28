547e257f0973c77a9734cc8210c49d97d775bb1c
JacksonJW <jacksonjwatkins@gmail.com>
2023-08-27 18:36:25 -0700

    8 kyu You Can't Code Under Pressure #1

---
+++ codewars/python/you_cant_code_under_pressure_#1.py
@@ -0,0 +1,18 @@
+# Code as fast as you can! You need to double the integer and return it.
+
+import codewars_test as test
+
+
+def double_integer(i):
+    return i * 2
+
+
+@test.describe("Tests")
+def tests():
+    @test.it("Sample Test Case")
+    def sample_test_case():
+        test.assert_equals(double_integer(2), 4)
+        test.assert_equals(double_integer(4), 8)
+        test.assert_equals(double_integer(-10), -20)
+        test.assert_equals(double_integer(0), 0)
+        test.assert_equals(double_integer(100), 200)
